5cc75aec2620c72118809c8321f7a826008e1842
Sinan Doğan <sinnandogann@outlook.com>
2025-06-07 21:36:00 +0300

    async and await2

---
+++ DERS6/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+#region Async Ve await
+
+async Task<int> X()
+{
+    return 33;
+}
+
+//BURADA DİKKAT ÇEKİLMEK İSTENEN 
+// İKİSİ ARASI FARK
+Task<int> Y()
+{
+    return 33;
+}
+#endregion
+
+
+
+#region Await example1
+ async  Task <string> ReadFile()
+{
+    StreamReader streamReader = new("Liste.txt");
+    string content=await streamReader.ReadToEndAsync();  
+    return content;
+      
+}
+//readfile dosyasının ıcerıgını okuduk
+string result = await ReadFile();
+Console.WriteLine(result);
+
+#endregion
